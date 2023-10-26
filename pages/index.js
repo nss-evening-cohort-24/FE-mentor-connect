a4e7682b33b6e1c8b6d6ba5022309974af18a671
cnfairall <101303134+cnfairall@users.noreply.github.com>
2023-10-25 19:56:38 -0500

    home page and flex

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,30 @@
-import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+/* eslint-disable @next/next/no-img-element */
+import meeting from '../public/assets/meeting.jpg';
+import { useAuth } from '../utils/context/authContext';
 
 function Home() {
   const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
 
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
+    <div id="home">
+      <h4 id="greeting">Hello {user.displayName}! </h4>
+      <div id="page-top">
+        <div>
+          <h1>Our Organization</h1>
+          <p className="text-block">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Proin sed libero enim sed faucibus turpis. Integer vitae justo eget magna fermentum. Facilisi etiam dignissim diam quis enim. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Urna duis convallis convallis tellus id interdum. Ultricies integer .</p>
+        </div>
+        <div>
+          <img src="/meeting.jpg" style={{ height: '300px' }} alt="two people meeting" />
+        </div>
+      </div>
+      <div id="page-bottom">
+        <img src={meeting} alt="two people meeting" />
+        <p className="text-block">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Proin sed libero enim sed faucibus turpis.</p>
+        <img src={meeting} alt="two people meeting" />
+        <p className="text-block">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Proin sed libero enim sed faucibus turpis.</p>
+        <img src={meeting} alt="two people meeting" />
+        <p className="text-block">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Proin sed libero enim sed faucibus turpis.</p>
+      </div>
     </div>
   );
 }
