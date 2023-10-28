763327e6d9a2029df9432dd302082a191d90f27b
cnfairall <101303134+cnfairall@users.noreply.github.com>
2023-10-28 13:02:48 -0500

    read appts

--- components/UserCard.js
+++ components/UserCard.js
@@ -1,19 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
+import { Card } from 'react-bootstrap';
 import { useAuth } from '../utils/context/authContext';
 
 // function show the authenticated user's profile info
-export default function UserProfile() {
+export default function UserCard() {
   const { user } = useAuth();
 
   return (
-    <div id="profile" className="user-page">
-      <img src={user.photoURL} alt="user" width="100px" height="100px" className="user-display-photo" />
-      <div className="infos"><br />
-        <h3 className="user-info"><b>Username:</b> {user.displayName}</h3>
-        <h6 className="user-info"><b>User E-mail: </b>{user.email}</h6>
-        <h6 className="user-info"><b>Last Login: </b>{user.metadata.lastSignInTime}</h6>
-      </div>
-    </div>
+    <>
+      <Card>
+        <img src={user.photoURL} alt="user" width="100px" height="100px" className="user-display-photo" />
+        <Card.Body>
+          <h6 className="user-info"><b>Username:</b> {user.displayName}</h6>
+          <h6 className="user-info"><b>User E-mail: </b>{user.email}</h6>
+          <h6 className="user-info"><b>Last Login: </b>{user.metadata.lastSignInTime}</h6>
+        </Card.Body>
+      </Card>
+    </>
   );
 }
--- pages/profile.js
+++ pages/profile.js
@@ -1,42 +1,30 @@
 /* eslint-disable @next/next/no-img-element */
-import { Card, Button } from 'react-bootstrap';
-import React from 'react';
-import UserProfile from '../components/UserCard';
-import { useAuth } from '../utils/context/authContext';
+import React, { useEffect, useState } from 'react';
+import UserCard from '../components/UserCard';
+import AppointmentCard from '../components/AppointmentCard';
+import getAppointments from '../api/appointmentData';
 
 export default function Profile() {
-  const { user } = useAuth();
+  const [appointments, SetAppointments] = useState([]);
+  const getAllAppointments = () => {
+    getAppointments().then(SetAppointments);
+  };
+
+  useEffect(() => (
+    getAllAppointments()
+  ), []);
 
   return (
     <>
-      <UserProfile />
       <div id="profile" className="user-page">
-        <Card>
-          <img src={user.photoURL} alt="user" width="100px" height="100px" className="user-display-photo" />
-          <Card.Body>
-            <h3>My Profile:</h3><br />
-            <h6 className="user-info"><b>Username:</b> {user.displayName}</h6>
-            <h6 className="user-info"><b>User E-mail: </b>{user.email}</h6>
-            <h6 className="user-info"><b>Last Login: </b>{user.metadata.lastSignInTime}</h6>
-          </Card.Body>
-        </Card>
-        <br />
-        <div id="apts">
-          <Card>
-            <div id="clock">
-              <img src="/clock.jpg" alt="icon" className="nav-logo" width="155" height="135" />
-            </div>
-            <Card.Body>
-              <h3>My Appointments:</h3><br />
-              <h6>Mentor Name</h6>
-              <h6>Category</h6>
-              <h6>Date/Time</h6><br />
-              <Button id="edit">EDIT</Button>
-              <Button id="memdel" className="m-2">
-                DELETE
-              </Button>
-            </Card.Body>
-          </Card>
+
+        <h3>My Profile</h3><br />
+        <UserCard />
+        <h3>My Appointments:</h3><br />
+        <div id="apt-container">
+          {appointments.map((apt) => (
+            <AppointmentCard appointmentObj={apt} key={apt.id} />
+          ))}
         </div>
       </div>
     </>
